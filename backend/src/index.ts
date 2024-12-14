871834cfbb0dc146f3c532025597c2794fffa55d
JuanRT <juan.m13@hotmail.fr>
2024-12-14 18:39:47 +0100

    messages model + routes + controllers done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db";
 import authRoutes from "./routes/auth.route";
+import messageRoutes from "./routes/message.route";
 
 dotenv.config()
 const app = express();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port: " + PORT);

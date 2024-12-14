70153129f5612e4b566e0fcab48120cc99f75b01
JuanRT <juan.m13@hotmail.fr>
2024-12-14 19:27:46 +0100

    setup app pages + axiosInstance + backend cors authorization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+import SignupPage from './pages/SignupPage'
+import { useAuthStore } from './store/useAuthStore'
 
-function App() {
+const App = () => {
+  const { checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
   return (
     <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
       <div className='text-green-700'>
         Hello worldies
       </div>
